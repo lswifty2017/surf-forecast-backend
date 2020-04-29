c2635ff878a87d55e7f7116c0c48aeea15eb503c
Liam <l.swift94@gmail.com>
2020-04-29 15:48:13 +1000

    First revision of swellnet scraper
    - Successfully obtained all data for sydney eastern beaches
    
    - Added momentJS

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const scrapeSwellnet = require('./scrapers/swellnet/swellnet-scraper');
 
 const app = express();
-const port = process.env.PORT || "8002";
+const port = process.env.PORT || '8002';
 
-app.get("/", (req, res) => {
-  res.status(200).send("Surf forecast backend");
+app.get('/', (req, res) => {
+  res.status(200).send('Surf forecast backend');
 });
+
+// Testing puppeteer
+
+scrapeSwellnet('australia', 'new-south-wales', 'eastern-beaches').then(res => {
+  console.log(res);
+});
+
+//
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
--- scrapers/swellnet/swellnet-scraper.js
+++ scrapers/swellnet/swellnet-scraper.js
@@ -1,10 +1,103 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+const scrapeSwellnet = async (country, region, beach) => {
+  try {
+    const pageURL = `https://www.swellnet.com/reports/${country}/${region}/${beach}/forecast`;
 
-  await browser.close();
-})();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(pageURL);
+
+    const swellForecast = await page.evaluate(() => {
+      const scrapedSwellData = [];
+
+      const forecastTableDays = document.querySelectorAll(
+        '.forecast_text_wrapper .day'
+      );
+
+      forecastTableDays.forEach(day => {
+        const date = day.querySelector('.summary .date_time_date').innerHTML;
+        const forecastTimes = day.querySelectorAll('div.time:not(.summary)');
+
+        forecastTimes.forEach(time => {
+          const forecastByTime = {
+            date: date
+          };
+
+          forecastByTime['time'] = time.querySelector('.date_time_time')
+            ? time.querySelector('.date_time_time').innerHTML
+            : null;
+
+          forecastByTime['surf_height'] = time.querySelector(
+            '.surf .scalar .amount'
+          )
+            ? time.querySelector('.surf .scalar .amount').innerHTML
+            : null;
+
+          forecastByTime['wind_direction'] = time.querySelector(
+            '.wind .direction'
+          )
+            ? time.querySelector('.wind .direction').innerHTML
+            : null;
+
+          forecastByTime['wind_speed'] = time.querySelector('.wind .speed')
+            ? time.querySelector('.wind .speed').innerHTML
+            : null;
+
+          forecastByTime['primary_swell_height'] = time.querySelector(
+            '.primary .amount'
+          )
+            ? time.querySelector('.primary .amount').innerHTML
+            : null;
+
+          forecastByTime['primary_swell_period'] = time.querySelector(
+            '.primary .period'
+          )
+            ? time.querySelector('.primary .period').innerHTML
+            : null;
+
+          forecastByTime['primary_swell_direction'] = time.querySelector(
+            '.primary .direction'
+          )
+            ? time.querySelector('.primary .direction').innerHTML
+            : null;
+
+          forecastByTime['secondary_swell_height'] = time.querySelector(
+            '.secondary .amount'
+          )
+            ? time.querySelector('.secondary .amount').innerHTML
+            : null;
+
+          forecastByTime['secondary_swell_period'] = time.querySelector(
+            '.secondary .period'
+          )
+            ? time.querySelector('.secondary .period').innerHTML
+            : null;
+
+          forecastByTime['secondary_swell_direction'] = time.querySelector(
+            '.secondary .direction'
+          )
+            ? time.querySelector('.secondary .direction').innerHTML
+            : null;
+
+          scrapedSwellData.push(forecastByTime);
+        });
+      });
+
+      return scrapedSwellData;
+    });
+
+    swellForecast.forEach(forecast => {
+      forecast['country'] = country;
+      forecast['region'] = region;
+      forecast['beach'] = beach;
+    });
+
+    await browser.close();
+    return swellForecast;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = scrapeSwellnet;
