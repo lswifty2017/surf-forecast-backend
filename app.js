5dd2e3586d44ba933f0d1f67ae37fc73cfec9947
Liam Swift <lswift@squiz.net>
2020-06-06 11:07:21 +1000

    Ready for test depoy @12pm

--- app.js
+++ app.js
@@ -4,34 +4,20 @@ const express = require('express');
 const cron = require('node-cron');
 const routes = require('./routes/index');
 const scrapeSwellnet = require('./scrapers/swellnet/swellnet-scraper');
-const updateSwellnetSheet = require('./lib/google-sheets/update-swellnet-sheet');
 
 const app = express();
 const port = process.env.DEV_PORT || '8002';
 
 app.use('/', routes);
 
-// cron.schedule('* * * * *', () => {
-//   console.log('running a task every minute');
-// });
-
-(async () => {
+cron.schedule('0 12 * * *', async () => {
   try {
     await scrapeSwellnet();
-    // await updateSwellnetSheet();
+    console.log('running a task every minute');
   } catch (err) {
     console.log(err);
   }
-})();
-
-// (async () => {
-//   try {
-
-//   }
-//   catch(err) {
-//     console.log(err)
-//   }
-// })();
+});
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
