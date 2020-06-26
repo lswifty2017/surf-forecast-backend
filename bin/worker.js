dee00f23f3f4a449bba366fb0032b655a7150124
Liam <l.swift94@gmail.com>
2020-06-26 21:29:17 +1000

    Destroy db data from 2 days old

--- bin/worker.js
+++ bin/worker.js
@@ -3,6 +3,13 @@ const scrapeSwellnet = require('../scrapers/swellnet/swellnet-scraper');
 (async () => {
   console.log('Cron jobs starting...');
   try {
+    await SwellnetForecasts.destroy({
+      where: {
+        createdAt: {
+          [Op.lte]: moment().subtract(2, 'days').toDate(),
+        },
+      },
+    });
     await scrapeSwellnet();
   } catch (err) {
     console.log(err);
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,4 @@ const router = express.Router();
 const swellnetForecastsController = require('../controllers/swellnet-forecasts-controller');
 
 router.get('/swellnet/forecasts', swellnetForecastsController.getAllForecasts);
-
 module.exports = router;
