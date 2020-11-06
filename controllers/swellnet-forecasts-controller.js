29fd9434b1d67446d14508d6ea89eef9208f9d36
Liam <l.swift94@gmail.com>
2020-11-06 11:20:26 +1100

    Added scrape endpoint to backend

--- controllers/swellnet-forecasts-controller.js
+++ controllers/swellnet-forecasts-controller.js
@@ -1,5 +1,6 @@
 const SwellnetSpreadsheet = require('../models/swellnet-spreadsheet');
 const SwellnetForecasts = require('../models/schemas/swellnet-forecasts');
+const scrapeSwellnet = require('../scrapers/swellnet/swellnet-scraper');
 
 exports.getAllForecasts = async (req, res) => {
   try {
@@ -10,5 +11,21 @@ exports.getAllForecasts = async (req, res) => {
     res.send(allForecasts);
   } catch (err) {
     res.send(err);
+  }
+};
+
+exports.scrapeAllForecasts = async () => {
+  try {
+    await SwellnetForecasts.destroy({
+      where: {
+        createdAt: {
+          [Op.lte]: moment().subtract(2, 'days').toDate(),
+        },
+      },
+    });
+    await scrapeSwellnet();
+    res.send('Scraping swellnet...');
+  } catch (err) {
+    console.log(err);
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const swellnetForecastsController = require('../controllers/swellnet-forecasts-controller');
 
 router.get('/swellnet/forecasts', swellnetForecastsController.getAllForecasts);
+router.get('/swellnet/scrape', swellnetForecastsController.scrapeAllForecasts);
 module.exports = router;
